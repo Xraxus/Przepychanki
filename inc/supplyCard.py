95251f49dfd67913efe9a33587df741d12ca1d5b
Xraxus <xraxus@gmail.com>
2023-02-11 22:22:08 +0100

    Revert "Complete phase(2) of claiming goods for both player's & speculant's pawns"
    This reverts commit 6ee863f839ff64d51025f7fd8f32c117cecfb6f5.

--- inc/supplyCard.py
+++
@@ -1,7 +0,0 @@
-class SupplyCard():
-    def __init__(self, category, amount):
-        self.category = category
-        self.amount = amount
-
-    def __repr__(self):
-        return '(Category: %s, Amount: %d)' % (self.category, self.amount)
